1f5b726547ec975e72b5b9417fa24b5107bdb3b0
Ayse Jones <aysej305@gmail.com>
2021-12-09 21:00:16 -0600

    server.js updated

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
-const app = require('express');
+const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const uuid = require('./uuid')
 
-const PORT = process. env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.Request(express.static('public'));
-app.Request(express.urlencoded({ extended: true }));
-app.Request(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/notes', (req, res) =>
+app.get('/', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, 'public/notes.html'))
+res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} post request recieved to add note`
+    )
+    const {title, text} = req.body
+    if (title && text) {
+        const newNote = {
+            title, 
+            text, 
+            id: uuid(),
+        }
+    }
+})
 
 app.listen(PORT, () =>
-console.log('App listening at http://localhost:${PORT}')
+console.log(`App listening at http://localhost:${PORT}`)
 );
