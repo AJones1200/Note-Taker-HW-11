98940c7c9624dcbf185361d6e0f451d254c44d14
Ayse Jones <aysej305@gmail.com>
2021-12-15 13:37:41 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const fs = require('fs');
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
 const path = require('path');
 const uuid = require('./uuid')
 
@@ -18,19 +20,40 @@ res.sendFile(path.join(__dirname, '/public/index.html'))
 app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-app.post('/api/notes', (req, res) => {
+
+app.get('/api/notes', async (req, res) => {
+    var notes = await readFileAsync('./db/db.json', 'utf8')
+    notes = JSON.parse(notes)
+    res.json(notes)
+}
+)
+
+app.post('/api/notes', async (req, res) => {
     console.info(`${req.method} post request recieved to add note`
     )
     const {title, text} = req.body
     if (title && text) {
+        var notes = await readFileAsync('./db/db.json', 'utf8')
+        notes = JSON.parse(notes)
+        console.log(notes)
         const newNote = {
             title, 
             text, 
             id: uuid(),
         }
-        const noteString = JSON.stringify(newNote);
-        fs.writeFile(``)
+        notes.push(newNote)
+
+        console.log(notes)
+        const noteString = JSON.stringify(notes);
+        fs.writeFile(`./db/db.json`, noteString, (err) => 
+        err ? console.log('err') : console.log(`note for ${newNote.title} has been written to JSON file.`));
+    const response = {
+        status:'success', 
+        body: newNote,
     }
+    res.status(201).json(response)
+}
+else {res.status(500).json(`Error in posting note.`)}
 })
 
 app.listen(PORT, () =>
